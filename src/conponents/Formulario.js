7bed486f0b8813145a68026f97ad449c442bb08b
Luis Valencia <luisvalencia.rincon@hotmail.com>
2021-08-18 15:41:21 -0500

    Agregando los campos del formulario

--- src/conponents/Formulario.js
+++ src/conponents/Formulario.js
@@ -6,7 +6,49 @@ const Formulario = () => {
             <h2>Crear Cita</h2>
 
             <form>
-                
+                <label>Nombre Mascota</label>
+                <input
+                    type="text"
+                    name="mascota" // 
+                    className="u-full-width"
+                    placeholder="Nombre de mascota"
+                />
+
+                <label>Nombre de Dueño</label>
+                <input 
+                    type="text"
+                    name="propietario"
+                    className="u-full-width"
+                    placeholder="Nombre de dueño"
+                />
+
+                <label>Fecha</label>
+                <input 
+                    type="date"
+                    name="fecha"
+                    className="u-full-width"
+                />
+
+                <label>Hora</label>
+                <input 
+                    type="time"
+                    name="hora"
+                    className="u-full-width"
+                />
+
+                <label>Sintomas</label>
+                <textArea
+                    type="text"
+                    name="sintomas"
+                    className="u-full-width"
+                    placeholder="Sintomas..."                
+                ></textArea>
+
+                <button
+                    type="submit"
+                    className="u-full-width button-primary"
+                >Agregar cita</button>
+
             </form>
         </Fragment>
      );
